50890ffca863f5fba4560bbb710b0c452b65e17c
nathan.lecorchet@epitech.eu <nathan.lecorchet@epitech.eu@epitech.eu>
2019-04-19 02:53:16 +0200

    Updating tetris header along with recent changes

--- lib/include/tetris.h
+++ lib/include/tetris.h
@@ -8,8 +8,12 @@
 #ifndef TETRIS_H_
 #define TETRIS_H_
 
+#include <sys/types.h>
 #include <stdbool.h>
 #include <getopt.h>
+
+/* displays help then leaves if it did */
+int display_help(int argc, char *argv[]);
 
 typedef int key_t;
 
@@ -30,7 +34,22 @@ struct game_props_s {
     struct key_settings_s key_codes;
 };
 
+/* modifies the tetris properties according to the arguments */
+int modify_game_props(int argc, char *argv[], struct game_props_s *game);
 extern const struct option optlist[];
 extern const struct game_props_s default_tetris;
+
+/* modifier_functions */
+struct game_changer_s {
+    int (* const change)(struct game_props_s *, char *);
+    int opt;
+};
+
+int change_next(struct game_props_s *game, char *arg);
+int change_level(struct game_props_s *game, char *arg);
+int change_debug(struct game_props_s *game, char *arg);
+int change_mapsize(struct game_props_s *game, char *arg);
+
+extern const struct game_changer_s args[];
 
 #endif /* !TETRIS_H_ */
