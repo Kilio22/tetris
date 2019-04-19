4ad0e3613034f54dce50186b0af491ac2db3a953
Kylian Balan <kylian.balan@epitech.eu>
2019-04-19 12:23:51 +0200

    yes

--- src/main.c
+++ src/main.c
@@ -10,18 +10,12 @@
 
 int main(int argc, char *argv[])
 {
-    int optindex;
-    int n_getopt;
     struct game_props_s game;
 
-    (void)argc;
-    (void)argv;
+    if (display_help(argc, argv))
+        return 0;
     init_game(&game);
-    n_getopt = getopt_long(argc, argv, "L:l:r:t:d:q:p:wD", optlist, &optindex);
-    while (n_getopt != -1) {
-        printf("%d[%c]\n", n_getopt, n_getopt);
-        n_getopt = getopt(argc, argv, "lu");
-    }
+    modify_game_props(argc, argv, &game);
     debug_mode(&game);
     return (0);
 }
