38f9789f741b17cd36e694d318e9c21453889064
Kylian Balan <kylian.balan@epitech.eu>
2019-05-27 17:57:26 +0200

    now using good variables

--- src/print_windows.c
+++ src/print_windows.c
@@ -7,28 +7,37 @@
 
 #include "tetris.h"
 
-void print_next(WINDOW *win, struct game_props_s *game)
+int get_next_tetriminos(bool oui, struct game_props_s *game)
 {
-    int i = 0;
-    int y = 1;
+    static int i = 0;
 
-    mvwprintw(win, 0, 2, "next");
+    if (!oui)
+        return i;
     while (game->tetriminos[i] && !game->tetriminos[i]->valid)
         ++i;
     if (i == game->nb_tetriminos)
-        return;
+        exit(84);
     while (1) {
         i = rand() % game->nb_tetriminos;
         if (game->tetriminos[i]->valid)
             break;
     }
+    return i;
+}
+
+void print_next(WINDOW *win, struct game_props_s *game)
+{
+    int i = get_next_tetriminos(false, game);
+    int y = 1;
+
+    mvwprintw(win, 0, 2, "next");
     for (size_t j = 0; game->tetriminos[i]->piece[0][j]; j++)
         mvwprintw(win, y++, 1, "%s", game->tetriminos[i]->piece[0][j]);
 }
 
 void print_score_board(WINDOW *win, struct game_props_s *game)
 {
-    mvwprintw(win, 2, 1, "High Score\t\t 0");
+    mvwprintw(win, 2, 1, "High Score\t\t %d", game->highscore);
     mvwprintw(win, 3, 1, "Score\t\t\t %d", game->score);
     mvwprintw(win, 4, 1, "Level\t\t\t %d", game->level);
 }
--- src/windows_managment.c
+++ src/windows_managment.c
@@ -9,14 +9,16 @@
 
 int create_windows(struct game_props_s *game)
 {
-    game->win[SCORE] = subwin(stdscr, 10, 40, 40, 0);
-    game->win[GAME] = subwin(stdscr, game->size[0], game->size[1], 0, 50);
+    game->win[SCORE] = subwin(stdscr, 10, 40, LINES - 11, 0);
+    game->win[GAME] = subwin(stdscr, game->size[0],
+game->size[1], LINES - game->size[0] - 1, 50);
     if (!game->win[SCORE] || !game->win[GAME])
         return -1;
     wborder(game->win[SCORE], '|', '|', '-', '-', '/', '\\', '\\', '/');
     wborder(game->win[GAME], '|', '|', '-', '-', '+', '+', '+', '+');
     if (!game->next) {
-        game->win[NEXT] = subwin(stdscr, 10, 20, 0, game->size[0] + 2 + 50);
+        game->win[NEXT] = subwin(stdscr, 10, 20, LINES - 11,
+game->size[1] + 4 + 50);
         if (!game->win[NEXT])
             return -1;
         wborder(game->win[NEXT], '|', '|', '-', '-', '/', '\\', '\\', '/');
@@ -28,15 +30,19 @@ int create_windows(struct game_props_s *game)
 
 void update_windows(struct game_props_s *game)
 {
+    clear();
     for (int i = 0; i < NB_WINDOW; i++)
         wclear(game->win[i]);
+    print_ascii_art(game);
     wborder(game->win[SCORE], '|', '|', '-', '-', '/', '\\', '\\', '/');
     print_score_board(game->win[SCORE], game);
     wborder(game->win[GAME], '|', '|', '-', '-', '+', '+', '+', '+');
+    print_game_board(game->win[GAME], game);
     if (!game->next) {
         wborder(game->win[NEXT], '|', '|', '-', '-', '/', '\\', '\\', '/');
         print_next(game->win[NEXT], game);
     }
     for (int i = 0; i < NB_WINDOW; i++)
         wrefresh(game->win[i]);
+    refresh();
 }
