691d9198f93591d1b728e6694dc18fc5ecdd11b7
Kylian Balan <kylian.balan@epitech.eu>
2019-05-29 16:30:01 +0200

    add update game

---
+++ src/update_game.c
@@ -0,0 +1,55 @@
+/*
+** EPITECH PROJECT, 2019
+** PSU_tetris_2018
+** File description:
+** update_game
+*/
+
+#include "tetris.h"
+
+static int update_tetrimono_pos(struct game_props_s *game, int i, size_t j)
+{
+    char tmp;
+
+    tmp = game->map[i][j].c;
+    game->map[i][j].id = 0;
+    game->map[i][j].c = ' ';
+    game->map[i + 1][j].color = game->map[i][j].color;
+    game->map[i][j].color = 0;
+    game->map[i + 1][j].id = 2;
+    game->map[i + 1][j].c = tmp;
+    return 0;
+}
+
+static int update_map(struct game_props_s *game, int i)
+{
+    for (size_t j = 0; j < game->size[1]; j++) {
+        if (game->map[i][j].id == 2)
+            update_tetrimono_pos(game, i, j);
+    }
+    return 0;
+}
+
+int my_find_char_inmap(struct map_s **map, int i,
+struct game_props_s *game, char c)
+{
+    for (size_t j = 0; j < game->size[1]; j++)
+        if (map[i][j].c == c && map[i][j].id == 2)
+            return j;
+    return -1;
+}
+
+int update_game(struct game_props_s *game)
+{
+    int n_return = 0;
+
+    n_return = check_full_piece(game);
+    if (n_return == -1 || n_return == 1)
+        return n_return;
+    for (int i = game->size[0] - 1; i >= 0; i--) {
+        n_return = update_map(game, i);
+        if (n_return == -1)
+            return -1;
+    }
+    return 0;
+}
