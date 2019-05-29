c6a3b03148c8e0a6d6ddfc8908b8326d8ba1d486
Kylian Balan <kylian.balan@epitech.eu>
2019-05-29 16:30:32 +0200

    add update for subwin positions

--- src/windows_managment.c
+++ src/windows_managment.c
@@ -10,8 +10,8 @@
 int create_windows(struct game_props_s *game)
 {
     game->win[SCORE] = subwin(stdscr, 10, 40, LINES - 11, 0);
-    game->win[GAME] = subwin(stdscr, game->size[0],
-game->size[1], LINES - game->size[0] - 1, 50);
+    game->win[GAME] = subwin(stdscr, game->size[0] + 2,
+game->size[1] + 2, LINES - game->size[0] - 2, 47);
     if (!game->win[SCORE] || !game->win[GAME])
         return -1;
     wborder(game->win[SCORE], '|', '|', '-', '-', '/', '\\', '\\', '/');
@@ -28,11 +28,23 @@ game->size[1] + 4 + 50);
     return 0;
 }
 
+void position_updates(struct game_props_s *game)
+{
+    for (int i = 0; i < NB_WINDOW; i++)
+        delwin(game->win[i]);
+    game->win[SCORE] = subwin(stdscr, 10, 40, LINES - 11, 0);
+    game->win[GAME] = subwin(stdscr, game->size[0] + 2,
+game->size[1] + 2, LINES - game->size[0] - 2, 47);
+    game->win[NEXT] = subwin(stdscr, 10, 20, LINES - 11,
+game->size[1] + 4 + 50);
+    for (int i = 0; i < NB_WINDOW; i++)
+        wclear(game->win[i]);
+}
+
 void update_windows(struct game_props_s *game)
 {
     clear();
-    for (int i = 0; i < NB_WINDOW; i++)
-        wclear(game->win[i]);
+    position_updates(game);
     print_ascii_art(game);
     wborder(game->win[SCORE], '|', '|', '-', '-', '/', '\\', '\\', '/');
     print_score_board(game->win[SCORE], game);
@@ -42,7 +54,7 @@ void update_windows(struct game_props_s *game)
         wborder(game->win[NEXT], '|', '|', '-', '-', '/', '\\', '\\', '/');
         print_next(game->win[NEXT], game);
     }
+    refresh();
     for (int i = 0; i < NB_WINDOW; i++)
         wrefresh(game->win[i]);
-    refresh();
 }
