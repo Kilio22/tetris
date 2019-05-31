2607f6c58fbabbeb12f881f6e3f9dc537d707d93
Kylian Balan <kylian.balan@epitech.eu>
2019-05-31 18:09:59 +0200

    add utils

--- src/utils.c
+++ src/utils.c
@@ -12,3 +12,21 @@ int my_error(char *str)
     my_dprintf(2, "%s\n", str);
     exit(84);
 }
+
+int my_find_char_inmap_rev(struct map_s **map, int i,
+struct game_props_s *game, char c)
+{
+    for (size_t j = game->size[1] - 1; j > 0; j--)
+        if (map[i][j].c == c && map[i][j].id == 2)
+            return j;
+    return -1;
+}
+
+int my_find_char_inmap(struct map_s **map, int i,
+struct game_props_s *game, char c)
+{
+    for (size_t j = 0; j < game->size[1]; j++)
+        if (map[i][j].c == c && map[i][j].id == 2)
+            return j;
+    return -1;
+}
